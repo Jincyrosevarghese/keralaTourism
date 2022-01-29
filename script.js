de7435c29d31e0d638dd79a94a2fc29a242a0e80
Jincy Varghese <jincyvar@gmail.com>
2022-01-29 17:18:27 +0530

    email fix

--- script.js
+++ script.js
@@ -2,12 +2,28 @@ function validate() {
 
     let phone = document.getElementById("phone");
     let error = document.getElementById("error");
+    let email = document.getElementById("email");
+    let error1 = document.getElementById("error1");
     let PasswordParameter = document.getElementById("pwd");
     let strengthBadge = document.getElementById('StrengthDisp');
     let phoneflag = false;
     let pwdflag = false;
-
+    let emailflag=false;
+    let emailregx=/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     let regex = /^([0-9]{3})([\.-\s]?)([0-9]{3})([\.-\s]?)([0-9]{4})$/;
+    if (emailregx.test(email.value)) {
+        error1.innerHTML = "email is valid";
+        error1.style.backgroundColor = "green";
+        error1.style.color = "white";
+        emailflag = true;
+    }
+    else {
+        error1.innerHTML = "email is not valid";
+        error1.style.backgroundColor = "red";
+        error1.style.color = "white";
+        //return false;
+    }
+
     if (regex.test(phone.value)) {
         error.innerHTML = "phone is valid";
         error.style.backgroundColor = "green";
@@ -35,7 +51,7 @@ function validate() {
         strengthBadge.style.backgroundColor = 'red';
         strengthBadge.textContent = 'Weak Password';
     }
-    if (phoneflag && pwdflag)
+    if (phoneflag && pwdflag &&emailflag)
         return true;
     else return false;
 }
